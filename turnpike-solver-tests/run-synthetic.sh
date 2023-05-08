f95823518f0ca89c8f5ddf8a2f81ba1e829938fe
wabiturnpike <132637975+wabiturnpike@users.noreply.github.com>
2023-05-08 18:25:17 -0400

    Update run-synthetic.sh

--- turnpike-solver-tests/run-synthetic.sh
+++ turnpike-solver-tests/run-synthetic.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
 # Create the runs directory if it doesn't exist
-mkdir -p runs
+mkdir -p synthetic
 
-# Loop through n values
-for n in $(seq 25 25 2000); do
-    # Loop through k values (convert to ε)
-    for k in $(seq -12 1 -1); do
-        # Loop through distribution types
-        for dist in normal uniform cauchy uint; do
-            # Generate a unique output filename based on n, k, and distribution
-            out_file="runs/run_n_${n}_k_${k}_${dist}.csv"
-
-            # Call the Python interface with the specified arguments
-            python run-turnpike-instance.py $n $k --distribution $dist --out $out_file
-        done
-    done
+for n in $(seq 25 25 100); do
+  # Runs n in range(25, 2025, 25) for eps in 10 ** -[1, 12] over all dists
+  python3.10 run-turnpike-instance.py $n 1 13 --distribution=all --out=synthetic --samples=10
 done
